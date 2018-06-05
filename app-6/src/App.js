1fd4b75e9d862fd09ef15e0d73ed1a88052bef9f
Goyo <gregory.vargas18@gmail.com>
2018-06-04 23:47:40 -0500

    completed question 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Todo from "./components/Todo";
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      myList: [],
+      userInput: ""
+    };
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.addToList = this.addToList.bind(this);
+  }
+
+  handleChange(event) {
+    // console.log(event.target.value);
+    this.setState({ userInput: event.target.value });
+  }
+
+  addToList() {
+    this.setState({
+      myList: this.state.myList.concat(this.state.userInput),
+      // myList: [...this.state.myList, this.state.userInput],
+      userInput: ""
+    });
+    console.log(this.state.myList);
+  }
+
   render() {
+    let newList = this.state.myList.map((val, i, arr) => {
+      return (
+        <Todo key={ i } task={ val } />
+      );
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <input onChange={ this.handleChange } />
+        <button onClick={ this.addToList }>Add</button>
+        { newList }
       </div>
     );
   }
